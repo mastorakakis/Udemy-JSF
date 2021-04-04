2f228ace5337665bf33c681e6500d40b0e8f23f5
Panagiotis Mastorakakis <panagiotismast@gmail.com>
2021-04-04 10:23:54 +0300

    Add 03-bean-dem, 04-list-and-table-demo

---
+++ 03-bean-demo/src/main/java/com/luv2code/jsf/beandemo/CounterTwo.java
@@ -0,0 +1,30 @@
+package com.luv2code.jsf.beandemo;
+
+import javax.faces.bean.ApplicationScoped;
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+@ApplicationScoped
+public class CounterTwo {
+
+	private int value = 0;
+
+	public String increment() {
+		value++;
+		
+		return "student_two";
+	}
+
+	public CounterTwo() {
+		
+	}
+	
+	public int getValue() {
+		return value;
+	}
+
+	public void setValue(int value) {
+		this.value = value;
+	}
+
+}
